8743870ff19313e40d5f56e85a34826830334efd
PrettyFlacko777 <aababochkin45@gmail.com>
2022-10-21 01:37:10 +0300

    next

--- sneakers/src/App.js
+++ sneakers/src/App.js
@@ -1,10 +1,30 @@
 
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    
+    <div className="wrapper">
+      <header>
+        <div className="header-left">
+          <svg />
+          <div className="header-info" >
+            <h1>REACT SNEAKERS</h1>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="header-right" >
+          <li>
+            <svg />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <svg />
+          </li>
+        </ul>
+      </header>
+      <div>
+        <h3>Все кроссовки</h3>
+        <span>.....</span>
+      </div>
     </div>
   );
 }
