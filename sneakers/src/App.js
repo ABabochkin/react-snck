012f7e1971765d5482830919ce4f5c2c552aea91
PrettyFlacko777 <aababochkin45@gmail.com>
2022-10-23 00:19:59 +0300

    add virtual backend and fetching items

--- sneakers/src/App.js
+++ sneakers/src/App.js
@@ -6,16 +6,28 @@ import { useState } from 'react'
 
 function App() {
 
-  const arr = [
+  /* const arr = [
     {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: '12 999 руб.', Image: './images/1.jpg'},
     {name: 'Мужские Кроссовки Nike Air Max 270', price: '12 999 руб.', Image: './images/2.jpg'},
     {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: '8 499 руб.', Image: './images/3.jpg'},
-    {name: 'Кроссовки Puma X Aka Boku Future Rider', price: '8 999 руб.', Image: './images/4.jpg'}
-]
+    {name: 'Кроссовки Puma X Aka Boku Future Rider', price: '8 999 руб.', Image: './images/4.jpg'},
+    {name: 'Мужские Кроссовки Under Armour Curry 8', price: '15 199 руб.', Image: './images/5.jpg'},
+    {name: 'Мужские Кроссовки Nike Kyrie 7', price: '11 299 руб.', Image: './images/6.jpg'},
+    {name: 'Мужские Кроссовки Jordan Air Jordan 11', price: '10 799 руб.', Image: './images/7.jpg'},
+    {name: 'Мужские Кроссовки Nike LeBron XVIII', price: '16 499 руб.', Image: './images/8.jpg'},
+] */
 
+
+  const [items, setItems] = useState([])
   const [cartOpened, setCartOpened] = useState(false)
 
-
+  fetch('https://63544c7ae64783fa8282d85a.mockapi.io/items')
+    .then(res => {
+      return res.json()
+    })
+    .then((json) => {
+      setItems(json)
+    } )
 
   return (
     <div className="wrapper" >
@@ -29,8 +41,8 @@ function App() {
             <input placeholder='Поиск...' />
           </div>
         </div>
-        <div style={{display: 'flex'}}>
-          {arr.map((obj) => (
+        <div style={{display: 'flex', flexWrap: 'wrap'}}>
+          {items.map((obj) => (
             <Card
               title = {obj.name}
               price = {obj.price}
