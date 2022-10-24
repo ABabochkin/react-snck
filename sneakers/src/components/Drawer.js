411aa5be811b4a6bd50415eca940e1661e4d5cd7
PrettyFlacko777 <aababochkin45@gmail.com>
2022-10-24 16:40:15 +0300

    add cart empty

--- sneakers/src/components/Drawer.js
+++ sneakers/src/components/Drawer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Drawer({onClose, items = [], onRemove}) {
 return (
-    <div className='overlay'>
+<div className='overlay'>
     <div className='drawer' >
         <div className='cartClose'>
             <h2>Корзина</h2>
@@ -10,38 +10,51 @@ return (
                 <img src='./images/remove.png' alt='close' />
             </button>
         </div>
-        <div className='items'>
 
-            {items.map((obj, id) => (
-            <div className='cardItem' key={id} >
-                <img style={{marginRight: '20px'}} width={70} height={70} src={obj.Image} alt='snk'/>
-                <div style={{marginRight: '20px'}} >
-                    <p>{obj.title}</p>
-                    <b>{obj.price}</b>
-                </div>
-                <button className='removeBtn' onClick={() => {onRemove(obj.id)} } > 
-                    <img width={11} height={11} src='./images/remove.png'  alt='removeBtn' />
-                </button>
-            </div>
+        {items.length ? (
+            <div className='items'>
+                {items.map((obj, id) => (
+                    <div className='cardItem' key={id} >
+                        <img style={{marginRight: '20px'}} width={70} height={70} src={obj.Image} alt='snk'/>
+                        <div style={{marginRight: '20px'}} >
+                            <p>{obj.title}</p>
+                            <b>{obj.price}</b>
+                        </div>
+                        <button className='removeBtn' onClick={() => {onRemove(obj.id)} } > 
+                            <img width={11} height={11} src='./images/remove.png'  alt='removeBtn' />
+                        </button>
+                    </div>
             ))}
-        </div>
-    <div className='cardTotalBlock'>
-        <ul>
-            <li>
-                <span>Итого:</span>
-                <div></div>
-                <b>21 498 руб. </b>
-            </li>
-            <li>
-                <span>Налог 5%: </span>
-                <div></div>
-                <b>1074 руб. </b>
-            </li>
-            <button className='buttonCheck'>Оформить заказ</button>
-            <img src='./images/btn.png'  alt='btn'/>
-        </ul>
+            </div>
+
+        ) : (
+
+            <div className='cartEmpty' >
+                <img width={120} height={120}  src='./images/cartEmpty.png' alt='cartEmpty'/>
+                <h2>Корзина пустая</h2>
+                <p>Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.</p>
+                <button className='greenButton' >  
+                    <img height={13} width={13} src='./images/buttonRotate.png' alt='btnRotate'/>  Вернуться назад 
+                </button>
+            </div>  
+        )}
+        <div className='cardTotalBlock'>
+            <ul>
+                <li>
+                    <span>Итого:</span>
+                    <div></div>
+                    <b>21 498 руб. </b>
+                </li>
+                <li>
+                    <span>Налог 5%: </span>
+                    <div></div>
+                    <b>1074 руб. </b>
+                </li>
+                <button className='buttonCheck'>Оформить заказ</button>
+                <img src='./images/btn.png'  alt='btn'/>
+            </ul>
+        </div>  
     </div>
-</div>
 </div>
 )
 }
